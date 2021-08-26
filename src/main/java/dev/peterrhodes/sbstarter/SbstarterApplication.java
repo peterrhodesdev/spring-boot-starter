c8bbf01fda87b5dfb3085f0e079a7372d6f7ebe1
prhodesy <peter.rhodes@gmail.com>
2021-08-26 16:44:12 +0700

    created project

---
+++ src/main/java/dev/peterrhodes/sbstarter/SbstarterApplication.java
@@ -0,0 +1,13 @@
+package dev.peterrhodes.sbstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbstarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/peterrhodes/sbstarter/SbstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.peterrhodes.sbstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbstarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
